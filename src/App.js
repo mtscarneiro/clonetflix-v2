374bf0f965c0343205f3341ed258700072d11b49
Matheus Carneiro <dev.mtscarneiro@gmail.com>
2022-10-11 15:59:14 -0400

    added firebase config and deployed, added routes and added redux for sign in

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 import HomeScreen from "./pages/HomeScreen";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route
-} from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from "./pages/Login";
+import { auth } from "./firebase/firebase";
+import { useDispatch, useSelector } from "react-redux";
+import { login, logout, selectUser } from "./features/userSlice";
 
 function App() {
-  const user = null;
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    return auth.onAuthStateChanged((userAuth) => {
+      if (userAuth) {
+        dispatch(login({
+          uid: userAuth.uid,
+          email: userAuth.email
+        }))
+      } else {
+        dispatch(logout)
+      }
+    })
+  }, [])
 
   return (
     <div className="app">
@@ -19,6 +32,7 @@ function App() {
         ) : (
           <Routes>
             <Route path={"/"} element={<HomeScreen />} />
+            <Route path={"/profile"} element={} />
           </Routes>
         )}
       </Router>
--- src/pages/SignInScreen/index.jsx
+++ src/pages/SignInScreen/index.jsx
@@ -1,14 +1,56 @@
-import React from "react";
+import React, { useRef } from "react";
 import "./SignInScreen.css"
+import { auth } from "../../firebase/firebase";
 
 const SignUpScreen = () => {
+  const emailRef = useRef(null)
+  const passwordRef = useRef(null)
+
+  const register = (event) => {
+    event.preventDefault()
+
+    auth.createUserWithEmailAndPassword(
+      emailRef.current.value,
+      passwordRef.current.value
+    ).then((authUser) => {
+      console.log(authUser)
+    }).catch((err) => {
+      alert(err.message)
+    })
+  }
+
+  const signIn = (event) => {
+    event.preventDefault()
+
+    auth.signInWithEmailAndPassword(
+      emailRef.current.value,
+      passwordRef.current.value
+    ).then((authUser) => {
+      console.log(authUser)
+    }).catch((err) => {
+      alert(err.message)
+    })
+  }
+
   return (
     <div className={"signUpScreen"}>
-      <form action="">
+      <form>
         <h1>Sign In</h1>
-        <input type="email" placeholder={"Email"} />
-        <input type="password" placeholder={"Password"} />
-        <button type="submit">Sign In</button>
+        <input ref={emailRef} type="email" placeholder={"Email"} />
+        <input ref={passwordRef} type="password" placeholder={"Password"} />
+        <button
+          type="submit"
+          onClick={signIn}
+        >
+          Sign In
+        </button>
+        <h4>New to Netflix?
+          <span
+            className={"signUpScreen_white"}
+            onClick={register}
+          > Sign Up now.
+          </span>
+        </h4>
       </form>
     </div>
   );
